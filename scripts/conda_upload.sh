83d418b1d5c1f91c795f6c586c3e161d144e1e1e
Tom Urbach <turbach@ucsd.edu>
2019-10-19 11:05:10 -0700

    path wrangling for conda_upload.sh

--- scripts/conda_upload.sh
+++ scripts/conda_upload.sh
@@ -4,26 +4,35 @@
 #
 # ${CONDA_PREFIX}/conda-bld/linux-65/sputdr-*.tar.bz
  
-# For testing, can fake the TravisCI env like so
+# For testing locally in an active conda env, can fake the TravisCI env like so
 #   export TRAVIS="true"
 #   export TRAVIS_BRANCH="X.Y.Z" 
+#   export ANACONDA_TOKEN="ku-actual-token ..."
+#
 
-if [[ "$TRAVIS" != "true" ]]
-then
+if [ $USER = "travis" ]; then
+    bld_prefix="/home/travis/miniconda"
+else
+    bld_prefix=${CONDA_PREFIX}
+fi
+
+
+if [ "$TRAVIS" != "true" ]; then
     echo "This is meant for a TravisCI build, do not run locally."
     exit -1
 fi
 
 # master branch builds get labeled for anacoda package "main"
 # others labeled by branch for testing
-if [[ $TRAVIS_BRANCH = "master" ]]; 
+if [ $TRAVIS_BRANCH = "master" ]; 
 then
     # do *NOT* force master onto main, we want version collisions to fail
     FORCE="" 
     conda_label="main"
 else
     # *DO* force non-master braches onto their label so we can conda install latest
-    # for testing
+    # for testing. Careful, this means non-master branches badly versioned as X.Y.Z 
+    # clobber main X.Y.Z
     FORCE="--force"  # 
     conda_label=latest$TRAVIS_BRANCH
 fi
@@ -35,22 +44,21 @@ echo "travis branch: $TRAVIS_BRANCH"
 echo "conda label: $conda_label"
 echo "force flag: $FORCE"
 
-
-# ok for a fresh TravisCI build but tar.bz2 files can accumulate during local testing
-bz_ls="/bin/ls -1 ${CONDA_PREFIX}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2"
+# not needed for travis but tar.bz2 files can accumulate during local testing
+bz_ls="/bin/ls -1 ${bld_prefix}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2"
 $bz_ls
 n_bz2=`${bz_ls} | wc -l`
 if (( $n_bz2 != 1 )); then
-    echo "found ${n_bz2} ${CONDA_PREFIX}/conda-bld/linux-64/${PACKAGE_NAME}"'-*.tar.bz2'
+    echo "found ${n_bz2} ${bld_prefix}/conda-bld/linux-64/${PACKAGE_NAME}"'-*.tar.bz2'
     echo "there must be exactly one to convert for the conda upload"
     exit -2
 fi
 
-# force convert b.c. of compiled C extension ... whatever works cross-platform works
+# force convert even thouhg compiled C extension ... whatever works cross-platform works
 rm -f -r ./tmp-conda-builds
 mkdir -p ./tmp-conda-builds/linux-64
-cp ${CONDA_PREFIX}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2 ./tmp-conda-builds/linux-64
-conda convert --platform all ${CONDA_PREFIX}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2 --output-dir ./tmp-conda-builds --force
+cp ${bld_prefix}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2 ./tmp-conda-builds/linux-64
+conda convert --platform all ${bld_prefix}/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2 --output-dir ./tmp-conda-builds --force
 /bin/ls -l ./tmp-conda-builds/**/${PACKAGE_NAME}-*.tar.bz2
 
 echo "Deploying to Anaconda.org like so ..."
