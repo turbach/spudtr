9a97681371d6899b08bcd5144557548ccff74f09
Qin <qiz001@ucsd.edu>
2019-11-04 16:45:20 -0800

    added test for filters

--- tests/test_epf.py
+++ tests/test_epf.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pytest
 import spudtr.epf as epf
 import spudtr.fake_epochs_data as fake_data
+import spudtr.filters as filters
 import numpy as np
 import pandas as pd
 
@@ -123,3 +124,83 @@ def test_re_reference_2(eeg_streams, rs, ref_type, expected):
     br_epochs_df = epf.re_reference(epochs_df, eeg_streams, rs, ref_type)
 
     assert list(br_epochs_df.b) == expected
+
+
+def test_lowpass_filter():
+
+    # creat a fakedata to show the filter
+    freq_list = [10, 30]
+    amplitude_list = [1.0, 1.0]
+    t, y = filters._sins_test_data(freq_list, amplitude_list)
+    testdata = pd.DataFrame({"fakedata": y})
+
+    ftype = "lowpass"
+    window = "kaiser"
+    cutoff_hz = 12.5
+    width_hz = 5
+    ripple_db = 60
+    sfreq = 250
+
+    filt_test_df = filters.epochs_filters(
+        testdata,
+        ["fakedata"],
+        ftype,
+        window,
+        cutoff_hz,
+        width_hz,
+        ripple_db,
+        sfreq,
+        trim_edges=False,
+    )
+    y_filt = filt_test_df["fakedata"]
+    freq_list = [10]
+    amplitude_list = [1.0]
+    t, y1 = filters._sins_test_data(freq_list, amplitude_list)
+    i1 = int(len(y1) / 2) - 20
+    i2 = int(len(y1) / 2) + 20
+    ya = y1[i1:i2]
+    yb = y_filt[i1:i2]
+    a = max(abs(ya - yb))
+    TorF = np.isclose(a, 0, atol=1e-02)
+    assert TorF == True
+
+
+def test_bandpass_filter():
+
+    # creat a fakedata to show the filter
+    freq_list = [10, 30, 60]
+    amplitude_list = [1.0, 1.0, 1.0]
+    t, y = filters._sins_test_data(freq_list, amplitude_list)
+    testdata = pd.DataFrame({"fakedata": y})
+
+    ftype = "bandpass"
+    window = "kaiser"
+    cutoff_hz = [22, 40]
+    width_hz = 5
+    ripple_db = 60
+    sfreq = 250
+
+    filt_test_df = filters.epochs_filters(
+        testdata,
+        ["fakedata"],
+        ftype,
+        window,
+        cutoff_hz,
+        width_hz,
+        ripple_db,
+        sfreq,
+        trim_edges=False,
+    )
+    y_filt = filt_test_df["fakedata"]
+
+    freq_list = [30]
+    amplitude_list = [1.0]
+    t, y1 = filters._sins_test_data(freq_list, amplitude_list)
+
+    i1 = int(len(y1) / 2) - 20
+    i2 = int(len(y1) / 2) + 20
+    ya = y1[i1:i2]
+    yb = y_filt[i1:i2]
+    a = max(abs(ya - yb))
+    TorF = np.isclose(a, 0, atol=1e-03)
+    assert TorF == True
